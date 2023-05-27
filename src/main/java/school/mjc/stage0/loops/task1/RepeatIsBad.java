19b7aa9d9f7f1ab7e5ea653da029bbc9d5897ebf
shandyz <shandyztop@hotmail.com>
2023-05-27 22:32:17 +0200

    done

--- src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
+++ src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
@@ -2,8 +2,15 @@ package school.mjc.stage0.loops.task1;
 
 public class RepeatIsBad {
 
+    int n = 20;
     @SuppressWarnings("java:S106")
-    public void repeatIsBad(){
+    public void repeatIsBad(int n){
 
+        String message = "writing the same code doesn't have much impact, and it's also time consuming";
+
+        if (n > 0) {
+            System.out.println(message);
+            repeatIsBad(n - 1);
+        }
     }
 }
